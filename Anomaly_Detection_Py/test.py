4cf5eb826352bcc1bf5156253e0ae3974e74b348
Meko27 <meihenkohsar@msn.com>
2020-02-04 15:07:21 +0100

    resolved typo

--- Anomaly_Detection_Py/test.py
+++ Anomaly_Detection_Py/test.py
@@ -5,6 +5,8 @@ import glob
 import ntpath
 from skimage.feature import hog
 import skimage
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from AnoDetector import AnoDetector
 from HOG_ground_dist import HOG_ground_dist
@@ -13,7 +15,7 @@ import time
 print('test successfull')
 path_normal = 'C:\\Users\\welschm_2\\Desktop\\MK\\Masterarbeit\\data\\carpet\\train\\good\\000.png'
 path_normal = '/mnt/c/Users/welschm_2/Desktop/MK/Masterarbeit/data/carpet/train/good/000.png'
-path normal = '/home/Meko/Repos/Master/Anomaly_Detection_Py/data/carpet/train/good/000.png'
+path_normal = '/home/Meko/Repos/Master/Anomaly_Detection_Py/data/carpet/train/good/000.png'
 img = cv2.imread(path_normal)
 print(img.shape)
 plt.imshow(img)
