bb9bfd34bec6fbe2f476a01506ad7678b85dbb54
Meko <kohsar@panda.technology>
2020-02-13 11:45:55 +0100

    fixed cropped error

--- Anomaly_Detection_Py/CarpetAno_cropped.py
+++ Anomaly_Detection_Py/CarpetAno_cropped.py
@@ -36,7 +36,7 @@ valid_img_type = '.png' # all images are type jpg
 img_addrs_list_normal = glob.glob(path_normal + '/*' + valid_img_type) #Windows: '\\*' , Mac, WSL: '/*
 img_addrs_list_anomal = glob.glob(path_anomal + '/*' + valid_img_type) #Windows: '\\* , Mac, WSL: '/*
 img_addrs_list = img_addrs_list_normal + img_addrs_list_anomal
-#img_addrs_list = img_addrs_list[:20] # TODO only for testing
+img_addrs_list = img_addrs_list[:20] # TODO only for testing
 img_id_list = []
 img_list = []
 hog_list = []
@@ -44,7 +44,7 @@ hog_list = []
 
 # Params
 # Cropping images
-crop_factor = 4
+SLICENUMBER = 4
 # Image scale
 img_scale = 1
 # HOG:
@@ -63,8 +63,9 @@ for i,addr in enumerate(img_addrs_list):
     img_id = img_id[:-len(valid_img_type)]
     img_id_list.append(img_id) # list of img names
     img = cv2.imread(addr)
-    #img = cv2.resize(img,tuple(int(img_scale * size) for size in img_size))
-    img = cv2.resize(img,(512,512))
+    img_size = img.shape[:2]
+    img = cv2.resize(img,tuple(int(img_scale * size) for size in img_size))
+    #img = cv2.resize(img,(512,512))
     img_size = img.shape[:2]
     img_list.append(img)
     print('load img {} of {}'.format(i+1,len(img_addrs_list)))
@@ -72,17 +73,23 @@ for i,addr in enumerate(img_addrs_list):
 
 img_cropped_list = []
 # Crop images
-SLICENUMBER = 16
 for img in img_list:
-    img_cropped = crop(img,SLICENUMBER)
-    img_cropped_list.append(img_cropped)
+    try:
+        img_cropped = crop(img,SLICENUMBER)
+        img_cropped_list.append(img_cropped)
+    except:
+        print('Cropfactor does not fit image size')
+
 
 
 # Calculate ground distance
 print('img loaded')
 print('calculate ground distance')
-ground_dist,d_hog = HOG_ground_dist(img_cropped_list[0][0],cell_size=pixels_per_cell, block_size=cells_per_block,block_stride=block_stride)
-print('ground_dist calculated, size:', ground_dist.shape)
+try:
+    ground_dist,d_hog = HOG_ground_dist(img_cropped_list[0][0],cell_size=pixels_per_cell, block_size=cells_per_block,block_stride=block_stride)
+    print('ground_dist calculated, size:', ground_dist.shape)
+except:
+    print('Grounddistance could not be calculated')
 
 # Extract Features
 hog_mat = np.empty((len(img_addrs_list), int(d_hog)))
