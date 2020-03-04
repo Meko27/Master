69139c68215b86d94e901c25aae14da3a3e8318d
Meko <kohsar@panda.technology>
2020-03-04 13:26:18 +0100

    object detection with contrast enhancement

--- Anomaly_Detection_Py/Profiles_Ano.py
+++ Anomaly_Detection_Py/Profiles_Ano.py
@@ -6,12 +6,55 @@ import os
 import glob
 import ntpath
 from skimage.feature import hog
-import skimage
+from skimage import exposure, img_as_float, img_as_ubyte
 import matplotlib.pyplot as plt
 from AnoDetector import AnoDetector
 from HOG_ground_dist import HOG_ground_dist
 import time
 #import pandas as pd
+
+
+def strech_contrast(img,A=0.15,B=0.65,SA=0.3,SB=0.9):
+    # Stech Contrast of Grayscale Image
+
+    img_float = img_as_float(img)  # Scale into range [0, 1] of float
+
+    mask_left = (img_float < A)
+    mask_center = ((img_float >= A) & (img_float < B))  
+    mask_right = (img_float >= B)
+
+    img_cont = np.copy(img_float)          
+    img_cont[mask_left]   = img_float[mask_left] * SA / A
+    img_cont[mask_center] = SA + (img_float[mask_center]-A) * (SB - SA) / (B - A)
+    img_cont[mask_right] = SB + (img_float[mask_right]-B) * (1. - SB) / (1. - B)
+    img_cont = img_as_ubyte(img_cont)
+    
+    return img_cont
+
+def contrast_bright_correction(img,ALPHA=2,BETA=20):
+    # Contrast and Brightness corrections  
+    
+    img_cont = np.zeros((img.shape[0],img.shape[1]),dtype = 'uint8')
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            img_cont[i,j] = np.clip(ALPHA*img[i,j]+BETA,0,255)  
+
+    return img_cont
+
+def detect_object(img , FILTER_LEN=800 , FILTER_WID=5):
+    # Detect Objectbounds
+    kernel = np.ones((FILTER_LEN,FILTER_WID), dtype=np.uint8)
+    img_centery = img.shape[0]//2 # vertical img center
+    pixel_pos = []
+    for i in range(FILTER_WID//2 + 1 , img.shape[1] - FILTER_WID//2):
+        result = kernel * img[img_centery - FILTER_LEN//2 : img_centery + FILTER_LEN//2 , i-FILTER_WID//2 : i+FILTER_WID//2 + 1 ]
+        if np.sum(result) < FILTER_WID*FILTER_LEN*200:
+            pixel_pos.append(i)            
+    edge_left = pixel_pos[0] + CROP_BOUNDX - 10
+    edge_right = pixel_pos[-1] + CROP_BOUNDX + 10
+    
+    return (edge_left,edge_right)
+
 
 ###########################################################
 ### Set Parameters
@@ -38,67 +81,60 @@ valid_img_type = 'bmp'
 img_addrs_list = glob.glob(path_data + '/*' + valid_img_type)
 #img_addrs_list = img_addrs_list[:20] # TODO only for testing
 img_ids = []
+img_orig_list = []
 img_list = []
+edges = []
+width_list = []
 # Load image and do preprocessing
-CROP_BOUNDX = 200 # precroppung to delete bounds
+CROP_BOUNDX = 300 # precroppung to delete bounds
 CROP_BOUNDY = 300
-FILTER_LEN = 100 # Object detection
-FILTER_WID = 5
-for i,addr in enumerate(img_addrs_list):
+CLIP_LIMIT = 0.015 # between (0,1) higher value -> higher contrast
+
+# Load images and detect edges
+for img_count,addr in enumerate(img_addrs_list):
     start = time.time()
+    # Load image
     img_id = ntpath.basename(addr)
     img_ids.append(img_id[(len(img_id)-6):-len(valid_img_type)])
     img = cv2.imread(addr)
+    img_orig_list.append(img)
 
-    # preprocessing 
+    # Preprocessing 
     img_g = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) # to gray
-    img_g_crop = img_g[CROP_BOUNDY:-CROP_BOUNDY , CROP_BOUNDX:-CROP_BOUNDX]
-        # contrast streching
-    ALPHA = 2
-    BETA = 20
-    img_cont = np.zeros((img_g_crop.shape[0],img_g_crop.shape[1]),dtype = 'uint8')
-    for i in range(img_g_crop.shape[0]):
-        for j in range(img_g_crop.shape[1]):
-            img_cont[i,j] = np.clip(ALPHA*img_g_crop[i,j]+BETA,0,255)
+    img_g = img_g[CROP_BOUNDY:-CROP_BOUNDY , CROP_BOUNDX:-CROP_BOUNDX] # crop out edges
+    img_g_cont = strech_contrast(img_g) # contrast streching
+
     # Detect Object
-    kernel = np.ones((FILTER_LEN,FILTER_WID), dtype=np.uint8)
-    img_centery = img_cont.shape[0]//2 # vertical img center
-    pixel_pos = []
-    for i in range(FILTER_WID//2 + 1 , img_cont.shape[1] - FILTER_WID//2):
-        result = kernel * img_cont[img_centery - FILTER_LEN//2 : img_centery + FILTER_LEN//2 , i-FILTER_WID//2 : i+FILTER_WID//2 + 1 ]
-        if np.sum(result) < FILTER_WID*FILTER_LEN*200:
-            pixel_pos.append(i)
-    print('min,max', (pixel_pos[0],pixel_pos[-1]))        
-    bound_left = pixel_pos[0] + CROP_BOUNDX - 10
-    bound_right = pixel_pos[-1] + CROP_BOUNDX + 10
-    img_out = img[:,bound_left:bound_right]
-    
-    # resize
+    edges.append(detect_object(img_g_cont)) 
+    width_list.append(edges[img_count][1] - edges[img_count][0])
+    end = time.time()
+    print('time loop1: ', end-start)
+
+width_max = np.max(np.array(width_list)) # max object width
+
+# Process and Crop images acc. to calculated edges
+for i,img in enumerate(img_orig_list):
+    start = time.time()
+    img_out = img[:,edges[i][0]:edges[i][0]+width_max,:]
+
+    # Contrast Limited Adaptive Histogram Equalizaton 
+    img_out = exposure.equalize_adapthist(img_out,clip_limit = CLIP_LIMIT) 
+    img_out = img_as_ubyte(img_out) # equalize_adapthist converst image to float64
+
+    # Resize
     img_width = img_out.shape[1]
     img_heigth = img_out.shape[0]
     img_re = cv2.resize(img_out,(img_width*RESIZE_FACTOR , img_heigth*RESIZE_FACTOR))
     img_list.append(img_re)
     print('load img {} of {}'.format(i+1,len(img_addrs_list)))
-    print('left bound: {}, right bound: {}'.format(bound_left,bound_right))
+    cv2.imwrite(str(i) + '.bmp',img_re)
     end=time.time()
-    print('time: ',end-start)
-    plt.subplot(1,4,1)
-    plt.imshow(img)
-    plt.title('original')
-    plt.subplot(1,4,2)
-    plt.imshow(img_cont,cmap='gray')
-    plt.title('contrasted imag')
-    plt.subplot(1,4,3)
-    plt.imshow(img_re)
-    plt.title('final cropped')
-    plt.subplot(1,4,4)
-    plt.imshow(img_out)
-    plt.title('out')
-    plt.show()
+    print('time loop 2: ', end-start)
 
+    
 # Calculate ground distance
 print('Calculate Ground Distane Matrix')
-ground_dist,d_hog = HOG_ground_dist(img_list[0],cell_size=pixels_per_cell, 
+ground_dist,d_hog = HOG_ground_dist(img_list[0],cell_size=pixels_per_cell,
                                     block_size=cells_per_block,
                                     block_stride=block_stride)
 print('Ground Distance Matrix calculated, size:', ground_dist.shape)
